287c0bfe11f9364f26a7d7236c32db85e6df6552
Chuck <chuckiepdelmundo@outlook.com>
2018-07-15 12:50:19 +0800

    broken code

--- app/src/main/java/com/example/chuckmarley/splashscreen/LoginActivity.java
+++ app/src/main/java/com/example/chuckmarley/splashscreen/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.chuckmarley.splashscreen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.chuckmarley.thesissecond.R;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/chuckmarley/thesissecond/MainActivity.java
+++ app/src/main/java/com/example/chuckmarley/thesissecond/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.chuckmarley.splashscreen.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
